20a43d3b557ed5b1a983a7f91bae83540b448ecf
theriley106 <christopherlambert106@gmail.com>
2019-05-01 14:40:32 -0400

    Added airbnb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import praw
+import re
+import json
+import bs4
 try:
 	from keys import *
 except:
@@ -11,6 +14,17 @@ def extract_from_reddit(idVal="9nb0vb"):
 	res = requests.get(url)
 	#print res.json()
 
+def extract_urls(fileName):
+	a = open(fileName).read()
+	url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
+	for match in url_re.finditer(a):
+		yield match.group(0)
+
+def extract_urls_from_text(a):
+	url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
+	for match in url_re.finditer(a):
+		yield match.group(0)
+
 def extract_all_comments(postID='914rhn'):
 	commentList = []
 	reddit = praw.Reddit(
@@ -18,12 +32,37 @@ def extract_all_comments(postID='914rhn'):
 		client_secret=REDDIT_CLIENT_SECRET,
 		user_agent='internship search')
 
-	submission = reddit.submission(id=)
+	submission = reddit.submission(id=postID)
 
 	submission.comments.replace_more(limit=None)
 	comment_queue = submission.comments[:]  # Seed with top-level
 	while comment_queue:
 		comment = comment_queue.pop(0)
-		commentList.append(comment.body)
+		lVal = []
+		for val in extract_urls_from_text(comment.body):
+			lVal.append(val)
+		commentList.append({"urls": lVal, "body": comment.body, "date": comment.created_utc})
 		comment_queue.extend(comment.replies)
 	return commentList
+
+def grabSite(url):
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+	return requests.get(url, headers=headers)
+
+def airbnb():
+	url = "https://careers.airbnb.com/university/"
+	res = grabSite(url)
+	page = bs4.BeautifulSoup(res.text, 'lxml')
+	return str(page).count("2020") > 0
+
+def do_all():
+	print(airbnb())
+
+
+if __name__ == '__main__':
+	do_all()
+	'''a = extract_all_comments()
+				with open('outputfile.json', 'w') as fout:
+					json.dump(a, fout, indent=4)'''
+	#for val in extract_urls('comments2.txt'):
+	#	print(val)
