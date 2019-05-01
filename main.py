4ef42744032e93d881e161cf6de6b89df6e61a73
theriley106 <christopherlambert106@gmail.com>
2019-05-01 13:41:57 -0400

    Added main

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import requests
+import praw
+try:
+	from keys import *
+except:
+	REDDIT_ID = input("REDDIT ID: ")
+	REDDIT_CLIENT_SECRET = input("REDDIT CLIENT SECRET: ")
 
 def extract_from_reddit(idVal="9nb0vb"):
 	url = "https://api.pushshift.io/reddit/submission/comment_ids/{}".format(idVal)
 	res = requests.get(url)
-	print res.json()
+	#print res.json()
 
-if __name__ == '__main__':
-	extract_from_reddit()
+import praw
+
+reddit = praw.Reddit(
+	client_id=REDDIT_ID,
+	client_secret=REDDIT_CLIENT_SECRET,
+	user_agent='internship search')
+
+submission = reddit.submission(id='914rhn')
+
+submission.comments.replace_more(limit=None)
+comment_queue = submission.comments[:]  # Seed with top-level
+while comment_queue:
+	comment = comment_queue.pop(0)
+	print(comment.body)
+	comment_queue.extend(comment.replies)
