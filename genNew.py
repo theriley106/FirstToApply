451e044d74ef40061f88b7290ffb7076f200c5a8
theriley106 <christopherlambert106@gmail.com>
2019-05-08 13:30:11 -0400

    Updated gen new page

--- genNew.py
+++ genNew.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import json
 
 driver = webdriver.Firefox()
 
@@ -11,10 +12,22 @@ def click_first_link():
 	results[0].click()
 
 if __name__ == '__main__':
+	db = json.load(open("links.json"))
 	while True:
 		company = input("Company: ")
 		if len(company) == 0:
 			driver.quit()
 			break
 		search(company + " internships")
+		click_first_link()
 		input("Press enter when done")
+		print("2020 Count: {}".format())
+		print("Intern Count: {}".format(str(driver.page_source).count('intern')))
+		info = {}
+		info['company'] = company
+		info['2020'] = str(driver.page_source).count('2020')
+		info['intern'] = str(driver.page_source).lower().count('intern')
+		info['url'] = driver.current_url
+		db.append(info)
+		with open('links.json', 'w') as outfile:
+		    json.dump(db, outfile)
