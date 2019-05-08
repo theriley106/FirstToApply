709c5155cdfb65ab5e4513ff276ec15f6e1bd81e
theriley106 <christopherlambert106@gmail.com>
2019-05-08 13:21:53 -0400

    Added gen New

--- genNew.py
+++ genNew.py
@@ -1,4 +1,20 @@
 from selenium import webdriver
 
+driver = webdriver.Firefox()
+
+
+def search(searchTerm):
+	driver.get("https://www.google.com/search?&q={}".format(searchTerm.replace(" ", "+")))
+
+def click_first_link():
+	results = driver.find_elements_by_xpath('//div[@class="r"]/a/h3')  # finds webresults
+	results[0].click()
+
 if __name__ == '__main__':
-	company = raw_input("Company: ")
+	while True:
+		company = input("Company: ")
+		if len(company) == 0:
+			driver.quit()
+			break
+		search(company + " internships")
+		input("Press enter when done")
