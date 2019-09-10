5acbd1b488bbcacdc0e422751268dd8c07964912
theriley106 <christopherlambert106@gmail.com>
2019-09-10 08:44:29 -0700

    Updated gen company json

--- genCompanyJson.py
+++ genCompanyJson.py
@@ -2,6 +2,15 @@ import json
 
 
 COMPANIES = open("companies.txt").read().split("\n")
+
+'''
+{
+	"ss_size": 0,
+	"count_2019": 0,
+	"count_intern": 0
+}
+
+'''
 if __name__ == '__main__':
 	for val in COMPANIES:
 		print(val)
