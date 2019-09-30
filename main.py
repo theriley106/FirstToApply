42ad839a9bd7d646ffd5084b341802270e09ecc4
theriley106 <christopherlambert106@gmail.com>
2019-09-30 08:53:12 -0700

    Removed reddit stuff from main.py

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 import requests
-import praw
 import re
 import json
 import bs4
 import os
 import time
 from selenium import webdriver
-try:
-	from keys import *
-except:
-	REDDIT_ID = ""
-	REDDIT_CLIENT_SECRET = ""
 
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
@@ -33,11 +27,6 @@ DB = json.load(open("db.json"))
 def extract_companies():
 	return json.load(open("links.json"))
 
-def extract_from_reddit(idVal="9nb0vb"):
-	url = "https://api.pushshift.io/reddit/submission/comment_ids/{}".format(idVal)
-	res = requests.get(url)
-	#print res.json()
-
 def extract_urls(fileName):
 	a = open(fileName).read()
 	url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
@@ -48,26 +37,6 @@ def extract_urls_from_text(a):
 	url_re = re.compile(r'\b(([\w-]+://?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/)))')
 	for match in url_re.finditer(a):
 		yield match.group(0)
-
-def extract_all_comments(postID='914rhn'):
-	commentList = []
-	reddit = praw.Reddit(
-		client_id=REDDIT_ID,
-		client_secret=REDDIT_CLIENT_SECRET,
-		user_agent='internship search')
-
-	submission = reddit.submission(id=postID)
-
-	submission.comments.replace_more(limit=None)
-	comment_queue = submission.comments[:]  # Seed with top-level
-	while comment_queue:
-		comment = comment_queue.pop(0)
-		lVal = []
-		for val in extract_urls_from_text(comment.body):
-			lVal.append(val)
-		commentList.append({"urls": lVal, "body": comment.body, "date": comment.created_utc})
-		comment_queue.extend(comment.replies)
-	return commentList
 
 def grabSite(url):
 	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
